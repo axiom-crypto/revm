759ee0a0982374a7e981e6a2ad94287d0e4ca319
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2024-12-08 23:31:06 -0500

    chore: update ecrecover for axvm-ecc-guest (#5)
    * chore: update ecrecover for new ecc guest
    
    * chore: rename

--- crates/precompile/src/secp256k1.rs
+++ crates/precompile/src/secp256k1.rs
@@ -11,29 +11,35 @@ pub use self::secp256k1::ecrecover;
 #[cfg(target_os = "zkvm")]
 #[allow(clippy::module_inception)]
 mod secp256k1 {
-    use axvm_ecc_guest::VerifyingKey;
+    use axvm_ecc_guest::{algebra::IntMod, ecdsa::VerifyingKey, weierstrass::WeierstrassPoint};
     use axvm_keccak256_guest::keccak256;
-    use k256::ecdsa::{Error, RecoveryId, Signature};
+    use k256::{
+        ecdsa::{Error, RecoveryId, Signature},
+        Secp256k1,
+    };
     use revm_primitives::{alloy_primitives::B512, B256};
 
     pub fn ecrecover(sig: &B512, mut recid: u8, msg: &B256) -> Result<B256, Error> {
         // parse signature
         let mut sig = Signature::from_slice(sig.as_slice())?;
-
-        // normalize signature and flip recovery id if needed.
         if let Some(sig_normalized) = sig.normalize_s() {
             sig = sig_normalized;
             recid ^= 1;
         }
         let recid = RecoveryId::from_byte(recid).expect("recovery ID is valid");
 
-        let recovered_key = VerifyingKey::recover_from_prehash(&msg[..], &sig, recid)?.0;
-        // hash it
-        let mut hash = keccak256(
-            &recovered_key
-                .to_encoded_point(/* compress = */ false)
-                .as_bytes()[1..],
+        // annoying: Signature::to_bytes copies from slice
+        let recovered_key = VerifyingKey::<Secp256k1>::recover_from_prehash_noverify(
+            &msg[..],
+            &sig.to_bytes(),
+            recid,
         );
+        let public_key = recovered_key.as_affine();
+        let mut encoded = [0u8; 64];
+        encoded.copy_from_slice(&public_key.x().to_be_bytes());
+        encoded[32..].copy_from_slice(&public_key.y().to_be_bytes());
+        // hash it
+        let mut hash = keccak256(&encoded);
         // truncate to 20 bytes
         hash[..12].fill(0);
         Ok(B256::from(hash))
