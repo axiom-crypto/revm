5a5bc385613c9fc84f80edd868dca24cbace3f13
Lun-Kai Hsu <kaikaikai1219@gmail.com>
2024-12-13 10:22:34 -0800

    [feat] precompile for ec add mul pairing (#7)
    * precompile for ec add mul pairing
    
    * use axvm feature
    
    * rename
    
    * pairing test runs
    
    * ec add test
    
    * it works, but why??
    
    * ec mul test
    
    * address comments
    
    * use openvm repo
    
    * address comments

---
+++ tests/openvm/programs/ec_add/src/main.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+
+use openvm::io::read_vec;
+use openvm_pairing_guest::bn254::Bn254Fp;
+use revm_precompile::bn128::{add::BYZANTIUM_ADD_GAS_COST, run_add};
+
+openvm::entry!(main);
+
+openvm_algebra_moduli_setup::moduli_init! {
+    "0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47",
+    "0x30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001"
+}
+
+openvm_ecc_sw_setup::sw_init! {
+    Bn254Fp,
+}
+
+pub fn main() {
+    let input = read_vec();
+    let expected = read_vec();
+
+    let outcome = run_add(&input, BYZANTIUM_ADD_GAS_COST, 500).unwrap();
+    assert_eq!(outcome.bytes, expected);
+}
