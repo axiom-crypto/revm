4dffb1cafd381c27a5e9a9e648df95554f9bd326
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2025-02-21 14:32:57 -0600

    chore: feature gate "openvm-kzg/use-intrinsics"

--- tests/openvm/programs/kzg_point_evaluation/src/main.rs
+++ tests/openvm/programs/kzg_point_evaluation/src/main.rs
@@ -7,23 +7,29 @@ use revm_precompile::kzg_point_evaluation::{run, GAS_COST};
 
 openvm::entry!(main);
 
+#[cfg(feature = "use-intrinsics")]
 openvm_algebra_guest::moduli_macros::moduli_init! {
     "0x1a0111ea397fe69a4b1ba7b6434bacd764774b84f38512bf6730d2a0f6b0f6241eabfffeb153ffffb9feffffffffaaab",
     "0x73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000001"
 }
 
+#[cfg(feature = "use-intrinsics")]
 openvm_algebra_guest::complex_macros::complex_init! {
     Bls12_381Fp2 { mod_idx = 0 },
 }
 
+#[cfg(feature = "use-intrinsics")]
 openvm_ecc_guest::sw_macros::sw_init! {
     Bls12_381G1Affine
 }
 
 pub fn main() {
-    setup_all_moduli();
-    setup_all_complex_extensions();
-    setup_all_curves();
+    #[cfg(feature = "use-intrinsics")]
+    {
+        setup_all_moduli();
+        setup_all_complex_extensions();
+        setup_all_curves();
+    }
 
     let input = read_vec();
     let expected_output = read_vec();
--- tests/openvm/tests/src/kzg_precompile.rs
+++ tests/openvm/tests/src/kzg_precompile.rs
@@ -17,10 +17,10 @@ use sha2::{Digest, Sha256};
 // These tests should be run with --profile=fast or --profile=ethtests for more compiler optimization
 
 #[test]
-fn test_kzg_precompile_basic() {
+fn test_kzg_precompile_with_intrinsics() {
     setup_tracing();
     let sdk = Sdk;
-    let guest_opts = GuestOptions::default();
+    let guest_opts = GuestOptions::default().with_features(["use-intrinsics"]);
     let mut pkg_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).to_path_buf();
     pkg_dir.push("../programs/kzg_point_evaluation");
     let elf = sdk.build(guest_opts, &pkg_dir, &None).unwrap();
@@ -48,6 +48,42 @@ fn test_kzg_precompile_basic() {
 
     // test data from: https://github.com/ethereum/c-kzg-4844/blob/main/tests/verify_kzg_proof/kzg-mainnet/verify_kzg_proof_case_correct_proof_31ebd010e6098750/data.yaml
 
+    let commitment = hex!("8f59a8d2a1a625a17f3fea0fe5eb8c896db3764f3185481bc22f91b4aaffcca25f26936857bc3a7c2539ea8ec3a952b7").to_vec();
+    let mut versioned_hash = Sha256::digest(&commitment).to_vec();
+    versioned_hash[0] = VERSIONED_HASH_VERSION_KZG;
+    let z = hex!("73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000000").to_vec();
+    let y = hex!("1522a4a7f34e1ea350ae07c29c96c7e79655aa926122e95fe69fcbd932ca49e9").to_vec();
+    let proof = hex!("a62ad71d14c5719385c0686f1871430475bf3a00f0aa3f7b8dd99a9abc2160744faf0070725e00b60ad9a026a15b1a8c").to_vec();
+
+    let input = [versioned_hash, z, y, commitment, proof].concat();
+
+    let expected_output = hex!("000000000000000000000000000000000000000000000000000000000000100073eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000001");
+
+    let mut io = StdIn::default();
+    io.write_bytes(&input);
+    io.write_bytes(&expected_output);
+    air_test_with_min_segments(vm_config, exe, io, 1);
+}
+
+#[test]
+fn test_kzg_precompile_without_intrinsics() {
+    setup_tracing();
+    let sdk = Sdk;
+    let guest_opts = GuestOptions::default();
+    let mut pkg_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).to_path_buf();
+    pkg_dir.push("../programs/kzg_point_evaluation");
+    let elf = sdk.build(guest_opts, &pkg_dir, &None).unwrap();
+
+    let vm_config = SdkVmConfig::builder()
+        .system(Default::default())
+        .rv32i(Default::default())
+        .rv32m(Default::default())
+        .io(Default::default())
+        .keccak(Default::default())
+        .build();
+    let exe = sdk.transpile(elf, vm_config.transpiler()).unwrap();
+
+    // test data from: https://github.com/ethereum/c-kzg-4844/blob/main/tests/verify_kzg_proof/kzg-mainnet/verify_kzg_proof_case_correct_proof_31ebd010e6098750/data.yaml
     let commitment = hex!("8f59a8d2a1a625a17f3fea0fe5eb8c896db3764f3185481bc22f91b4aaffcca25f26936857bc3a7c2539ea8ec3a952b7").to_vec();
     let mut versioned_hash = Sha256::digest(&commitment).to_vec();
     versioned_hash[0] = VERSIONED_HASH_VERSION_KZG;
