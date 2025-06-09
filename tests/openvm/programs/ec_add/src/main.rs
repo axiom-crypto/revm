4f9fd27d49fa3904d69c4525a6c930bd95abb292
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2025-06-09 17:52:27 +0200

    fixup

--- tests/openvm/programs/ec_add/src/main.rs
+++ tests/openvm/programs/ec_add/src/main.rs
@@ -3,7 +3,7 @@
 
 use openvm::io::read_vec;
 #[allow(unused_imports)]
-use openvm_pairing_guest::bn254::Bn254G1Affine;
+use openvm_pairing::bn254::Bn254G1Affine;
 use revm_precompile::bn128::{add::BYZANTIUM_ADD_GAS_COST, run_add};
 
 openvm::init!();
@@ -11,8 +11,6 @@ openvm::init!();
 openvm::entry!(main);
 
 pub fn main() {
-    setup_all_moduli();
-    setup_all_curves();
     let input = read_vec();
     let expected = read_vec();
 
